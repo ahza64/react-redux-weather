b78643f2ae9375f34785b12c2747636ee300681c
ahza64 <ahza64@yahoo.com>
2016-11-19 00:08:48 -0800

    set up weather api req/res

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import API_KEY from '../../key.js';
+import axios from 'axios';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY.key}`;
+
+console.log(API_KEY.key);
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather (city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
